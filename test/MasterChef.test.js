cea4563eb224b229eff6001aef49257543e19c4f
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2020-11-13 09:34:33 +0000

    should allow the dev and only dev to update dev

--- test/MasterChef.test.js
+++ test/MasterChef.test.js
@@ -5,7 +5,9 @@ const SushiToken = artifacts.require('SushiToken')
 const MasterChef = artifacts.require('MasterChef')
 const MockERC20 = artifacts.require('MockERC20')
 
-    
+let sushi
+let chef    
+
 contract('MasterChef', ([ owner, user1, user2, minter, dev ]) => {
     beforeEach(async () => {
         this.sushi = await SushiToken.new({ from: owner })
@@ -21,10 +23,18 @@ contract('MasterChef', ([ owner, user1, user2, minter, dev ]) => {
         const sushiOwner = await this.sushi.owner()
         assert.equal(sushi.valueOf(), this.sushi.address)
         assert.equal(devaddr.valueOf(), dev)
-        assert.equal(sushiOwner.valueOf(), this.chef.address)
-        console.log(this.sushi.address)
-        
-        
-        
+        assert.equal(sushiOwner.valueOf(), this.chef.address) 
     })
+
+    it('should allow the dev and only dev to update dev', async () => {
+        this.chef = await MasterChef.new(this.sushi.address, dev, '1000', '0', '1000', { from: owner})
+        assert.equal((await this.chef.devaddr()).valueOf(), dev)
+        await expectRevert(this.chef.dev(user1, { from: user1}), 'dev: wut?')
+        await this.chef.dev(user1, { from: dev})
+        assert.equal((await this.chef.devaddr()).valueOf(), user1)
+        await this.chef.dev(owner, { from: user1 })
+        assert.equal((await this.chef.devaddr()).valueOf(), owner)        
+    })
+
+
 })
